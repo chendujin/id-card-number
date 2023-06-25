39765023f97d4484ae7a7562faf29c6a4010d32e
thomas.chen <chendujin@zujibao.net>
2023-06-25 19:51:32 +0800

    commit init

--- src/IdCardNumber.php
+++ src/IdCardNumber.php
@@ -158,6 +158,87 @@ class IdCardNumber
     }
 
     /**
+     * 根据身份证号，自动返回对应的星座
+     * @return string
+     */
+    public function getIDCardXZ(): string
+    {
+        $bir = substr($this->id,10,4);
+        $month = (int)substr($bir,0,2);
+        $day = (int)substr($bir,2);
+        $strValue = '';
+        if(($month == 1 && $day <= 21) || ($month == 2 && $day <= 19)) {
+            $strValue = "水瓶座";
+        } elseif (($month == 2 && $day > 20) || ($month == 3 && $day <= 20)) {
+            $strValue = "双鱼座";
+        } elseif (($month == 3 && $day > 20) || ($month == 4 && $day <= 20)) {
+            $strValue = "白羊座";
+        } elseif (($month == 4 && $day > 20) || ($month == 5 && $day <= 21)) {
+            $strValue = "金牛座";
+        } elseif (($month == 5 && $day > 21) || ($month == 6 && $day <= 21)) {
+            $strValue = "双子座";
+        } elseif (($month == 6 && $day > 21) || ($month == 7 && $day <= 22)) {
+            $strValue = "巨蟹座";
+        } elseif (($month == 7 && $day > 22) || ($month == 8 && $day <= 23)) {
+            $strValue = "狮子座";
+        } elseif (($month == 8 && $day > 23) || ($month == 9 && $day <= 23)) {
+            $strValue = "处女座";
+        } elseif (($month == 9 && $day > 23) || ($month == 10 && $day <= 23)) {
+            $strValue = "天秤座";
+        } elseif (($month == 10 && $day > 23) || ($month == 11 && $day <= 22)) {
+            $strValue = "天蝎座";
+        } elseif (($month == 11 && $day > 22) || ($month == 12 && $day <= 21)) {
+            $strValue = "射手座";
+        } elseif (($month == 12 && $day > 21) || ($month == 1 && $day <= 20)) {
+            $strValue = "魔羯座";
+        }
+        return $strValue;
+    }
+
+    /**
+     * 根据身份证号，自动返回对应的生肖
+     * @return string
+     */
+    public function getIDCardSX(): string
+    {
+        $start = 1901;
+        $end = (int)substr($this->id,6,4);
+        $x = ($start - $end) % 12;
+        $value = "";
+        if($x == 1 || $x == -11){$value = "鼠";}
+        if($x == 0) {$value = "牛";}
+        if($x == 11 || $x == -1){$value = "虎";}
+        if($x == 10 || $x == -2){$value = "兔";}
+        if($x == 9 || $x == -3){$value = "龙";}
+        if($x == 8 || $x == -4){$value = "蛇";}
+        if($x == 7 || $x == -5){$value = "马";}
+        if($x == 6 || $x == -6){$value = "羊";}
+        if($x == 5 || $x == -7){$value = "猴";}
+        if($x == 4 || $x == -8){$value = "鸡";}
+        if($x == 3 || $x == -9){$value = "狗";}
+        if($x == 2 || $x == -10){$value = "猪";}
+        return $value;
+    }
+
+    /**
+     * 根据身份证号，自动返回对应的省、自治区、直辖市代
+     * @return string
+     */
+    public function getProvince(): string
+    {
+        $index = substr($this->id,0,2);
+        $area = array(
+            11 => "北京",  12 => "天津", 13 => "河北",   14 => "山西", 15 => "内蒙古", 21 => "辽宁",
+            22 => "吉林",  23 => "黑龙江", 31 => "上海",  32 => "江苏",  33 => "浙江", 34 => "安徽",
+            35 => "福建",  36 => "江西", 37 => "山东", 41 => "河南", 42 => "湖北",  43 => "湖南",
+            44 => "广东", 45 => "广西",  46 => "海南", 50 => "重庆", 51 => "四川", 52 => "贵州",
+            53 => "云南", 54 => "西藏", 61 => "陕西", 62 => "甘肃", 63 => "青海", 64 => "宁夏",
+            65 => "新疆", 71 => "台湾", 81 => "香港", 82 => "澳门", 91 => "国外"
+        );
+        return $area[$index];
+    }
+
+    /**
      * 获取行政代码数组
      * @return array
      */
