f4b1a5bc72f9d20be8fff8643a93914676dd005f
thomas.chen <chendujin@zujibao.net>
2023-06-25 20:49:10 +0800

    commit init

---
+++ Facades/IdCardNumber.php
@@ -0,0 +1,21 @@
+<?php
+// +----------------------------------------------------------------------
+// | 云盾风控审批系统
+// +----------------------------------------------------------------------
+// | Version: v1.0.0
+// +----------------------------------------------------------------------
+// | Author: 托马斯 <chendujin@zujibao.net>
+// +----------------------------------------------------------------------
+// | Date: 2023/6/25
+// +----------------------------------------------------------------------
+namespace Chendujin\IdCardNumber\Facades;
+
+use Illuminate\Support\Facades\Facade;
+
+class IdCardNumber extends Facade
+{
+    protected static function getFacadeAccessor(): string
+    {
+        return 'id_card_number';
+    }
+}
